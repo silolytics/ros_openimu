47106dfa918828680c768376c6ff769eb876ea49
tiesus <t.junge@silolytics.de>
2024-02-06 20:52:20 +0100

    Add aceinna package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
-    packages=['ros_openimu'],
+    packages=['ros_openimu', 'aceinna'],
     package_dir={'': 'src'}
 )
 setup(**d)
