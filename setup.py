2e9119183e4658ea83bbaff34720ccb5284d94e9
tiesus <t.junge@silolytics.de>
2024-02-07 13:04:36 +0100

    Add rtk

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['ros_openimu', 'aceinna.bootstrap', 'aceinna.devices', 'aceinna.devices.base',
-              'aceinna.devices.openimu', 'aceinna.devices.dmu', 'aceinna.devices.configs', 'aceinna.devices.parsers', 'aceinna.devices.upgrade_workers',
+              'aceinna.devices.openimu','aceinna.devices.openrtk', 'aceinna.devices.dmu', 'aceinna.devices.configs', 'aceinna.devices.parsers', 'aceinna.devices.upgrade_workers',
               'aceinna.framework', 'aceinna.framework.utils', 'aceinna.models', 'aceinna.setting', 'aceinna.tools'],
     package_dir={'': 'src'},
 )
